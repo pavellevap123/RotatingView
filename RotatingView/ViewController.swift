484a5229f4ccb440f393f9ff1edc284ce71e3ed8
Pavel Paddubotski <pavel.paddubotski@gmail.com>
2023-05-07 23:14:56 +0200

    Make transition using slider

--- RotatingView/ViewController.swift
+++ RotatingView/ViewController.swift
@@ -8,12 +8,88 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private lazy var containerView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .cyan
+        return view
+    }()
+    
+    private lazy var rotatingView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .systemBlue
+        view.layer.cornerRadius = 8
+        return view
+    }()
+    
+    private lazy var slider: UISlider = {
+        let slider = UISlider()
+        slider.translatesAutoresizingMaskIntoConstraints = false
+        slider.minimumValue = 0
+        slider.maximumValue = 1
+        return slider
+    }()
+    
+    var trailingConstraint: NSLayoutConstraint!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        layout()
+        
+        slider.addTarget(self, action: #selector(sliderValueChanged), for: .valueChanged)
     }
-
-
+    
+    private func layout() {
+        view.addSubview(containerView)
+        containerView.addSubview(rotatingView)
+        view.addSubview(slider)
+        
+        NSLayoutConstraint.activate([
+            // containerView
+            containerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 48),
+            containerView.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor),
+            containerView.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor),
+            containerView.heightAnchor.constraint(equalToConstant: 140),
+            
+            // rotatingView
+            rotatingView.centerYAnchor.constraint(equalTo: containerView.centerYAnchor),
+            //rotatingView.leadingAnchor.constraint(equalTo: containerView.leadingAnchor),
+            rotatingView.heightAnchor.constraint(equalToConstant: 80),
+            rotatingView.widthAnchor.constraint(equalTo: rotatingView.heightAnchor),
+            
+            // slider
+            slider.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor),
+            slider.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor),
+            slider.topAnchor.constraint(equalTo: containerView.bottomAnchor, constant: 48)
+        ])
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        distance = containerView.bounds.width - ((rotatingView.bounds.width / 2) * 2) + (rotatingView.bounds.width / 2 * 0.5)
+        rotatingView.center = CGPoint(x: rotatingView.bounds.width / 2, y: containerView.bounds.height / 2)
+    }
+    
+    var distance: CGFloat = 0
+    
+    @objc func sliderValueChanged(_ sender: UISlider) {
+        
+        let scaleFactor = 1 + (sender.value * 0.5)
+        let angle = sender.value * 90
+        
+        let distance = CGFloat(sender.value) * self.distance
+        
+        UIView.animate(withDuration: 0.2, animations: {
+            if distance < self.rotatingView.bounds.width / 2 {
+                self.rotatingView.center.x = self.rotatingView.bounds.width / 2
+            } else {
+                self.rotatingView.center.x = distance
+            }
+            
+            self.rotatingView.transform = CGAffineTransform(rotationAngle: CGFloat(angle) * CGFloat.pi / 180).concatenating(CGAffineTransform(scaleX: CGFloat(scaleFactor), y: CGFloat(scaleFactor)))
+        })
+    }
 }
 
