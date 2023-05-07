c711fb492ae4ef1f3a2f2cf9ae927af59af9075b
Pavel Paddubotski <pavel.paddubotski@gmail.com>
2023-05-07 10:52:22 +0200

    Initial Commit

---
+++ RotatingView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RotatingView
+//
+//  Created by Pavel Paddubotski on 7.05.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
